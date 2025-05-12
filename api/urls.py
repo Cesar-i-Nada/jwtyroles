d4b791149ebb3d744a3dded1e8d42694a6c0cf23
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-05-12 05:11:25 -0600

    actualpostviews

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ClienteListCreateView, ClienteDetailView,ProveedorDetailView,ProveedorListCreateView, ProductoDetailView,ProductoListCreateView
+from .views import ClienteListCreateView, ClienteDetailView,ProveedorListCreateView, ProveedorDetailView,ProductoListCreateView,ProductoDetailView, EmpleadoListCreateView,EmpleadoDetailView,MetodoPagoListCreateView, MetodoPagoDetailView, PedidoListCreateView,PedidoDetailView, PedidoProductoListCreateView, PedidoProductoDetailView
 
 urlpatterns = [
     path('cliente/', ClienteListCreateView.as_view(), name='cliente-listar-crear'),
@@ -8,4 +8,12 @@ urlpatterns = [
     path('proveedor/<int:pk>/', ProveedorDetailView.as_view(), name='proveedor-editar-actualizar'), 
     path('producto/', ProductoListCreateView.as_view(), name='producto-listar-crear'),
     path('producto/<int:pk>/', ProductoDetailView.as_view(), name='producto-editar-actualizar'), 
+    path('empleado/', EmpleadoListCreateView.as_view(), name='empleado-listar-crear'),
+    path('empleado/<int:pk>/', EmpleadoDetailView.as_view(), name='empleado-editar-actualizar'),
+    path('metodoPago/', MetodoPagoListCreateView.as_view(), name='metodoPago-listar-crear'),
+    path('metodoPago/<int:pk>/', MetodoPagoDetailView.as_view(), name='metodoPago-editar-actualizar'),
+    path('pedido/', PedidoListCreateView.as_view(), name='pedido-listar-crear'),
+    path('pedido/<int:pk>/', PedidoDetailView.as_view(), name='pedido-editar-actualizar'),
+    path('pedidoProducto/', PedidoProductoListCreateView.as_view(), name='pedidoProducto-listar-crear'),
+    path('pedidoProducto/<int:pk>/', PedidoProductoDetailView.as_view(), name='pedidoProducto-editar-actualizar'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Cliente, Producto, Proveedor, Empleado, MetodoPago, Pedido, PedidoProducto
+from .serializers import ClienteSerializer, ProductoSerializer, ProveedorSerializer, EmpleadoSerializer, MetodoPagoSerializer, PedidoSerializer, PedidoProductoSerializer
 
-# Create your views here.
+class ClienteListCreateView(ListCreateAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer 
+    
+class ClienteDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    
+class ProductoListCreateView(ListCreateAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    
+class ProductoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    
+class ProveedorListCreateView(ListCreateAPIView):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+    
+class ProveedorDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+    
+class EmpleadoListCreateView(ListCreateAPIView):
+    queryset = Empleado.objects.all()
+    serializer_class = EmpleadoSerializer
+    
+class EmpleadoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Empleado.objects.all()
+    serializer_class = EmpleadoSerializer
+    
+class MetodoPagoListCreateView(ListCreateAPIView):
+    queryset = MetodoPago.objects.all()
+    serializer_class = MetodoPagoSerializer
+    
+class MetodoPagoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = MetodoPago.objects.all()
+    serializer_class = MetodoPagoSerializer
+    
+class PedidoListCreateView(ListCreateAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+    
+class PedidoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+    
+class PedidoProductoListCreateView(ListCreateAPIView):
+    queryset = PedidoProducto.objects.all()
+    serializer_class = PedidoProductoSerializer
+    
+class PedidoProductoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = PedidoProducto.objects.all()
+    serializer_class = PedidoProductoSerializer    
